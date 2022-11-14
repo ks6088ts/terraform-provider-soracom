b2d902676194cc8877969e17b6c5885de3c00ae4
ks6088ts <ks6088ts@gmail.com>
2022-11-14 16:03:36 +0900

    add Azure resources

---
+++ examples/group_configuration_funk_azure_functions_js/variables.tf
@@ -0,0 +1,11 @@
+variable "azure_prefix" {
+  description = "Prefix"
+  type        = string
+  default     = "azsrcm"
+}
+
+variable "azure_resource_location" {
+  description = "Location"
+  type        = string
+  default     = "japaneast"
+}
